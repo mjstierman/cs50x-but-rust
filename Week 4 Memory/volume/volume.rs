bfff9e2045ddf9a478eb4c35e76773a5fb3126b6
Matthew Stierman <matthew.stierman@alumni.du.edu>
2025-03-26 23:44:55 -0600

    Compiles, reads the file.

--- Week 4 Memory/volume/volume.rs
+++ Week 4 Memory/volume/volume.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::io;
 use std::process::ExitCode;
 
 // Number of bytes in .wav header
@@ -8,24 +9,22 @@ const HEADER_SIZE: u32 = 44;
 fn main() -> ExitCode {
     // Check command-line arguments
     let args: Vec<String> = env::args().collect();
-    if args.len() != 4
-    {
+    if args.len() != 4 {
         println!("Usage: ./volume input.wav output.wav factor\n");
         return ExitCode::from(1);
     }
 
-    // Open files and determine scaling factor
-    let file = fs::read(&args[1]);
+    // Open the input file
+    let mut file = match fs::File::open(&args[1]) {
+        Ok(file) => file,
+        Err(e) => {
+            println!("Error opening file: {}", e);
+            return ExitCode::from(1);
+        }
+    };
     
-    //let mut output = fs::write(&args[2]).expect("Could not open file.");
-    let factor: f32 = args[3].trim().parse().expect("Not a number.");
-
     // TODO: Copy header from input file to output file
-
     // TODO: Read samples from input file and write updated data to output file
 
-    // No need to close files in Rust
-
-    return ExitCode::SUCCESS
+    return ExitCode::SUCCESS;
 }
-
