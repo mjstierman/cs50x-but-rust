5b23d323a6ecc313e485a7b578e199815779ceae
Matthew Stierman <matthew.stierman@alumni.du.edu>
2025-04-01 17:17:46 -0600

    Week 4: Volume complete

--- Week 4 Memory/volume/volume.rs
+++ Week 4 Memory/volume/volume.rs
@@ -1,10 +1,10 @@
 use std::env;
 use std::fs;
-use std::io;
+use std::io::Write;
 use std::process::ExitCode;
 
 // Number of bytes in .wav header
-const HEADER_SIZE: u32 = 44;
+const HEADER_SIZE: usize = 44;
 
 fn main() -> ExitCode {
     // Check command-line arguments
@@ -13,18 +13,43 @@ fn main() -> ExitCode {
         println!("Usage: ./volume input.wav output.wav factor\n");
         return ExitCode::from(1);
     }
+    
+    // Validate third arg is an int
+    let factor: u8 = match args[3].trim().parse() {
+        Ok(factor) => factor,
+        Err(e) => {
+            println!("Error getting factor: {}", e);
+            return ExitCode::from(1);
+        }
+    };
 
     // Open the input file
-    let mut file = match fs::File::open(&args[1]) {
-        Ok(file) => file,
+    let input_file: Vec<u8> = match fs::read(&args[1]) {
+        Ok(input_file) => input_file,
         Err(e) => {
             println!("Error opening file: {}", e);
             return ExitCode::from(1);
         }
     };
-    
+
+    // Create the output file
+    let mut output_file = match fs::File::create_new(&args[2]) {
+        Ok(output_file) => output_file,
+        Err(e) => {
+            println!("Error creating new file: {}", e);
+            return ExitCode::from(1);
+        }
+    };
+
     // TODO: Copy header from input file to output file
-    // TODO: Read samples from input file and write updated data to output file
+    output_file.write_all(&input_file[..HEADER_SIZE]).expect("Failed to write header.");
+
+    // Read samples from input file and write updated data to output file
+    let samples = &input_file[HEADER_SIZE..];
+    let updated_samples: Vec<u8> = samples.iter()
+        .map(|&byte| byte.wrapping_add(factor)) // Modify each byte
+        .collect();
+    output_file.write_all(&updated_samples).expect("Failed to write header.");
 
     return ExitCode::SUCCESS;
 }
